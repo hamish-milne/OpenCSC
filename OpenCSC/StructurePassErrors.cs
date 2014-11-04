816663363b5f9b9fc621c4960c5f07c39e2a853c
hamish-milne <hammil@theovermare.com>
2014-11-04 18:26:34 +0000

    For some reason, VS didn't want to push these files

---
+++ OpenCSC/StructurePassErrors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenCompiler;
+
+namespace OpenCSC
+{
+	public class UnexpectedKeyword : DefaultCompilerError
+	{
+		public override int Number
+		{
+			get { return 1041; }
+		}
+
+		public Keyword Keyword;
+
+		public override Substring Message
+		{
+			get
+			{
+				return Keyword == null ? "Identifier expected" :
+					("Identifier expected; '" + Keyword.Value + "' is a keyword");
+			}
+		}
+
+		public UnexpectedKeyword(int line, int column, int length, Keyword keyword)
+			: base(line, column, length)
+		{
+			Keyword = keyword;
+		}
+
+		public UnexpectedKeyword(TokenInfo item)
+			: base(item)
+		{
+			Keyword = item.Item as Keyword;
+		}
+	}
+}
---
+++ OpenCompiler/StructurePass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCompiler
+{
+	public interface IScopeModifier
+	{
+		void RunScopeModifier(StructurePass parent);
+	}
+
+	public abstract class StructurePass : Pipeline<IList<TokenInfo>, IList<TypeStructure>>
+	{
+		public abstract IList<Substring> Scope { get; }
+
+		public abstract IList<Substring> Aliases { get; }
+
+		public abstract TokenInfo this[int i] { get; }
+
+		public virtual void Advance()
+		{
+			Advance(1);
+		}
+
+		public abstract void Advance(int i);
+
+		public abstract void RegisterCloseBrace(Brace open);
+	}
+}
